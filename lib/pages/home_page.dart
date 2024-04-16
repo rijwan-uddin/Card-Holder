aa9a17f121b2473512a7d84514797d197e6ea651
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-04-16 21:44:51 +0600

    NavigationBar added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,66 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Contact List'),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        shape: const CircleBorder(),
+        child: const Icon(Icons.add),
+
+      ),
+      bottomNavigationBar:  BottomAppBar(
+        padding:EdgeInsets.zero,
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAlias,
+        child:  BottomNavigationBar(
+          
+          onTap: (index){
+            setState(() {
+              selectedIndex= index;
+            });
+
+          },
+          currentIndex: selectedIndex,
+
+          backgroundColor: Colors.blue[100],
+          items:  const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'All'
+            ),
+
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favorite'
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
